0904fc95dc38112075427ec8620740a0e29a6d8e
Matt Penn <mattpenn114@gmail.com>
2024-11-09 16:17:49 +0000

    refactor

--- sherlock/model/iterate_folders.py
+++ sherlock/model/iterate_folders.py
@@ -1,11 +1,26 @@
 from .config import EnvSettings
 from .sherlock import process_folder
+import os
+from pathlib import Path
 
 ENV = EnvSettings()
 
-
-def run_sherlock():
+def run_sherlock(root_path:str = ENV.root_directory):
     """
     Run Sherlock on a range of folders.
+
+    Args:
+        root_path (str): The root path
     """
-    root_path = ENV.root_directory
+    # Run folder if it meets conditions
+    folder_path = Path(root_path)
+    file_count = len(list(folder_path.glob(f"*{ENV.image_suffix}")))
+    if file_count > ENV.min_images_process and folder_path.split('/')[-1] != 'positive_images':
+        print(f'Running folder {root_path}')
+        process_folder(root_path)
+    else:
+        # Process subfolders
+        for folder in os.listdir(root_path):
+            run_sherlock(f"{root_path}/{folder}")
+
+    
